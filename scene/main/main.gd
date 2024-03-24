28e16efc51809d59547f2e54d9c0c5c3ec5062e9
Zelefin <nagangoroh@gmail.com>
2024-03-24 16:55:40 +0200

    car death + dead screen

--- scene/main/main.gd
+++ scene/main/main.gd
@@ -1,8 +1,8 @@
 extends Node
 
 @onready var car = $Car
-
 @onready var pause_menu = %PauseMenu
+
 var paused = false
 
 const VALUE_NEEDED_TO_CHANGE_ROAD = 32
@@ -14,30 +14,45 @@ var target_rotation: float = 0
 var move_speed: float = 0
 var rotate_speed: float = 0
 
+var dead_wait = false
+
 func _ready():
 	pass
+
+
+func wait_and_die():
+	if not dead_wait:
+		await get_tree().create_timer(1).timeout
+		dead_wait = true
+	get_tree().change_scene_to_file("res://scene/deadScreen/dead_scene.tscn")
+
 
 func _process(delta):
 	if Input.is_action_just_pressed("Pause"):
 		pauseManu()
 	elif Input.is_action_just_pressed("MoveRight") and car.position.x < 120:
+		if not car.can_move:
+			wait_and_die()
+			return
 		target_position_x = car.position.x + VALUE_NEEDED_TO_CHANGE_ROAD
 		move_speed = (VALUE_NEEDED_TO_CHANGE_ROAD / TIME_NEEDED_TO_CHANGE_ROAD)
 		target_rotation = 15
 		rotate_speed = (15 / TIME_NEEDED_TO_CHANGE_ROAD)
-	elif Input.is_action_just_pressed("MoveLeft") and car.position.x > 32:
+	elif Input.is_action_just_pressed("MoveLeft") and car.position.x > 62:
+		if not car.can_move:
+			wait_and_die()
+			return
 		target_position_x = car.position.x - VALUE_NEEDED_TO_CHANGE_ROAD
 		move_speed = (VALUE_NEEDED_TO_CHANGE_ROAD / TIME_NEEDED_TO_CHANGE_ROAD)
 		target_rotation = -15
 		rotate_speed = (15 / TIME_NEEDED_TO_CHANGE_ROAD)
 
-	
-	if move_speed > 0 and !paused:
+	if move_speed > 0:
 		car.position.x = lerp(car.position.x, target_position_x, move_speed * delta)
 		car.rotation_degrees = lerp(car.rotation_degrees, target_rotation, rotate_speed * delta)
 
 		# Check if the car has reached the target rotation
-		if abs(car.rotation_degrees - target_rotation) < 1.0 and !paused:
+		if abs(car.rotation_degrees - target_rotation) < 1.0:
 			# Reset target rotation and position
 			target_rotation = 0
 			target_position_x = car.position.x  # You may need to adjust this depending on your game logic
@@ -52,4 +67,3 @@ func pauseManu():
 		Engine.time_scale = 0
 	
 	paused = !paused
-	
