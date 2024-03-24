670661e8101fb38e7eea7510502a0ab47ec5dec3
Anteon1939 <164662944+Anteon1939@users.noreply.github.com>
2024-03-24 14:13:23 +0200

    points add

--- player_car/car.gd
+++ player_car/car.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+var point =0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,5 +8,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	point+=30*delta
 	pass
 
