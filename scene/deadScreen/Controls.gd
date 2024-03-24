d2412b9b56f3275de7f0cfd938274af52c455d9e
Zelefin <nagangoroh@gmail.com>
2024-03-24 22:16:04 +0200

    added scene transitions

--- scene/deadScreen/Controls.gd
+++ scene/deadScreen/Controls.gd
@@ -1,10 +1,16 @@
 extends Control
 
+@onready var scene_trasition: CanvasLayer = $"../SceneTrasition"
+
 func _on_retry_tsb_pressed():
 	Global.point = 0
-	get_tree().change_scene_to_file("res://scene/main/main.tscn")
+	scene_trasition.transition()
 
 
 func _on_menu_tsb_pressed():
 	Global.point = 0
 	get_tree().change_scene_to_file("res://scene/mainMenu/mainMenu.tscn")
+
+
+func _on_scene_trasition_transitioned() -> void:
+	get_tree().change_scene_to_file("res://scene/main/main.tscn")
