581fc5e7fdb67d468e5437edb7bd204bd9da117e
Maxsonin <maximles.lv@gmail.com>
2024-03-25 17:46:58 +0200

    updated movement

--- scene/main/main.gd
+++ scene/main/main.gd
@@ -2,53 +2,44 @@ extends Node
 
 @onready var engine_sound = %engineSound
 
-@onready var car = $Car
 @onready var pause_menu = %PauseMenu
-@onready var scene_trasition: CanvasLayer = $SceneTrasition
-
 var paused = false
 
+@onready var scene_trasition: CanvasLayer = $SceneTrasition
+
+@onready var turn_anim = %turnAnim
+@onready var car = $Car
 const VALUE_NEEDED_TO_CHANGE_ROAD = 32
 const TIME_NEEDED_TO_CHANGE_ROAD = 1
-
 var target_position_x: float = 0
-var target_rotation: float = 0
-
 var move_speed: float = 0
-var rotate_speed: float = 0
-
 
 func _ready():
 	pass
 
-
 func _process(delta):
 	if Input.is_action_just_pressed("Pause"):
 		pauseManu()
+
 	elif Input.is_action_just_pressed("MoveRight") and car.position.x < 120:
 		if not car.can_move: return
+		turn_anim.play("turn_right")
 		target_position_x = car.position.x + VALUE_NEEDED_TO_CHANGE_ROAD
 		move_speed = (VALUE_NEEDED_TO_CHANGE_ROAD / TIME_NEEDED_TO_CHANGE_ROAD)
-		target_rotation = 15
-		rotate_speed = (15 / TIME_NEEDED_TO_CHANGE_ROAD)
+
 	elif Input.is_action_just_pressed("MoveLeft") and car.position.x > 62:
 		if not car.can_move: return
+		turn_anim.play("turn_left")
 		target_position_x = car.position.x - VALUE_NEEDED_TO_CHANGE_ROAD
 		move_speed = (VALUE_NEEDED_TO_CHANGE_ROAD / TIME_NEEDED_TO_CHANGE_ROAD)
-		target_rotation = -15
-		rotate_speed = (15 / TIME_NEEDED_TO_CHANGE_ROAD)
 
 	if move_speed > 0:
 		car.position.x = lerp(car.position.x, target_position_x, move_speed * delta)
-		car.rotation_degrees = lerp(car.rotation_degrees, target_rotation, rotate_speed * delta)
-
-		# Check if the car has reached the target rotation
-		if abs(car.rotation_degrees - target_rotation) < 1.0:
-			# Reset target rotation and position
-			target_rotation = 0
-			target_position_x = car.position.x  # You may need to adjust this depending on your game logic
-			move_speed = (VALUE_NEEDED_TO_CHANGE_ROAD / TIME_NEEDED_TO_CHANGE_ROAD)  # Reset move speed as well
-			corig()
+		
+	if move_speed == 0:
+		target_position_x = car.position.x 
+		move_speed = (VALUE_NEEDED_TO_CHANGE_ROAD / TIME_NEEDED_TO_CHANGE_ROAD)
+		corig()
 
 func pauseManu():
 	if paused:
@@ -62,22 +53,23 @@ func pauseManu():
 	
 	paused = !paused
 
-
 func _on_car_car_got_hit_fr() -> void:
 	await get_tree().create_timer(1).timeout
 	scene_trasition.transition()
 	#get_tree().change_scene_to_file("res://scene/deadScreen/dead_scene.tscn")
 
-
 func _on_scene_trasition_transitioned() -> void:
 	get_tree().change_scene_to_file("res://scene/deadScreen/dead_scene.tscn")
 
 func corig():
-	if car.position.x>133 and car.position.x>113:
-		car.position.x=133
-	if (car.position.x>27 and car.position.x<=57):
-		car.position.x=32
-	if (car.position.x>57 and car.position.x<=80):
-		car.position.x=63
-	if (car.position.x>80 and car.position.x<=113):
+	if car.position.x > 133 and car.position.x>113:
+		car.position.x = 128
+		
+	if car.position.x > 27 and car.position.x <= 57:
+		car.position.x = 32
+		
+	if car.position.x > 57 and car.position.x <= 80:
+		car.position.x = 63
+		
+	if car.position.x > 80 and car.position.x <= 113:
 		car.position.x=96
