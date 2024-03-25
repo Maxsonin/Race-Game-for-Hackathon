e3384d9bf64e0220f4f069a27b4547308dbf2e8e
Zelefin <nagangoroh@gmail.com>
2024-03-25 22:01:01 +0200

    added new obstacle cars to spawn

--- obstacles/obstacles_generator.gd
+++ obstacles/obstacles_generator.gd
@@ -2,21 +2,35 @@ extends Node2D
 
 @export var CarObstacle: PackedScene
 @export var SignObstacle: PackedScene
+@export var TaxiObstacle: PackedScene
+@export var SemiObstacle: PackedScene
+@export var JeepObstacle: PackedScene
+@export var PickupObstacle: PackedScene
 
 var screen_width = ProjectSettings.get_setting("display/window/size/viewport_width")
 var margine = 8
 var spawn_points = [32, 64, 96, 128]
 var prev_picked_by_random = 0
+var prev_prev_picked_by_random = 0
 var boo = 0
 
 @onready var spawner: Node2D = $Spawner
 @onready var car_obst_timer: Timer = $CarObstTimer
 @onready var sign_obst_timer: Timer = $SignObstTimer
+@onready var taxi_obst_timer: Timer = $TaxiObstTimer
+@onready var semi_obst_timer: Timer = $SemiObstTimer
+@onready var jeep_obst_timer: Timer = $JeepObstTimer
+@onready var pickup_obst_timer: Timer = $PickupObstTimer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	car_obst_timer.timeout.connect(spawn_shi.bind(CarObstacle, car_obst_timer))
 	sign_obst_timer.timeout.connect(spawn_shi.bind(SignObstacle, sign_obst_timer))
+	taxi_obst_timer.timeout.connect(spawn_shi.bind(TaxiObstacle, taxi_obst_timer))
+	semi_obst_timer.timeout.connect(spawn_shi.bind(SemiObstacle, semi_obst_timer))
+	jeep_obst_timer.timeout.connect(spawn_shi.bind(JeepObstacle, jeep_obst_timer))
+	pickup_obst_timer.timeout.connect(spawn_shi.bind(PickupObstacle, pickup_obst_timer))
 
 
 func _process(delta):
@@ -30,7 +44,9 @@ func spawn_shi(obstacle: PackedScene, timer: Timer) -> void:
 	spawner.scene = obstacle
 	boo = spawn_points.pick_random()
 	while boo == prev_picked_by_random:
+		if boo == prev_prev_picked_by_random: continue
 		boo = spawn_points.pick_random()
 	prev_picked_by_random = boo
+	prev_prev_picked_by_random = 0
 	spawner.spawn(Vector2(boo, -30))
 	timer.start()
