46ae9f53c32bae55f5ad1c402205b04bdd5acde1
Maxsonin <maximles.lv@gmail.com>
2024-03-25 18:20:00 +0200

    added shop Button

--- scene/mainMenu/Controls.gd
+++ scene/mainMenu/Controls.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var engine_sound = $engineSound
+@onready var engine_sound = %engineSound
 @onready var scene_trasition: CanvasLayer = $"../SceneTrasition"
 
 func _on_play_tsb_pressed():
@@ -17,3 +17,7 @@ func _on_quite_tsb_pressed():
 
 func _on_scene_trasition_transitioned() -> void:
 	get_tree().change_scene_to_file("res://scene/main/main.tscn")
+
+
+func _on_store_tsb_pressed():
+	get_tree().change_scene_to_file("res://scene/garage/garage.tscn")
