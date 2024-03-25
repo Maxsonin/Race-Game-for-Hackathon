a10300312ef44606eb112522295eb0869bf68ff6
Zelefin <nagangoroh@gmail.com>
2024-03-25 22:47:01 +0200

    smooth transition to menu (from dead screen)

--- scene/deadScreen/Controls.gd
+++ scene/deadScreen/Controls.gd
@@ -2,16 +2,20 @@ extends Control
 
 @onready var scene_trasition: CanvasLayer = $"../SceneTrasition"
 
+var retrying = true
+
 func _on_retry_tsb_pressed():
 	scene_trasition.transition()
 
 
 func _on_menu_tsb_pressed():
-	Global.point = 0
-	get_tree().change_scene_to_file("res://scene/mainMenu/mainMenu.tscn")
+	retrying = false
+	scene_trasition.transition()
 
 
 func _on_scene_trasition_transitioned() -> void:
 	Global.point = 0
-	get_tree().change_scene_to_file("res://scene/main/main.tscn")
-	
+	if retrying:
+		get_tree().change_scene_to_file("res://scene/main/main.tscn")
+	else:
+		get_tree().change_scene_to_file("res://scene/mainMenu/mainMenu.tscn")
