8067b9e291478383651dd687f9cd155c2f687850
Zelefin <nagangoroh@gmail.com>
2024-03-26 09:56:47 +0200

    better spawning (for night time), transition to dead sceen truned back on

--- scene/main/main.gd
+++ scene/main/main.gd
@@ -72,10 +72,11 @@ func _on_car_car_got_hit_fr() -> void:
 	sound_in_out_controller.play("out")
 	await get_tree().create_timer(1).timeout
 	scene_trasition.transition()
-	#get_tree().change_scene_to_file("res://scene/deadScreen/dead_scene.tscn")
+
 
 func _on_scene_trasition_transitioned() -> void:
 	get_tree().change_scene_to_file("res://scene/deadScreen/dead_scene.tscn")
+
 
 func setPosition():
 	if pos == -1:
